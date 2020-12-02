f685dc62aaf07a3a7c7350b4b3ad8524ee4830e9
TonyStark992 <maxime.castillo@gmail.com>
2020-12-02 04:00:38 +0100

    Scaffold d'Articles avec un seed de la database Fixes #1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Création de quelques articles
+8.times do
+  article = Article.create!(title: Faker::Lorem.sentence(word_count: 2), content: Faker::ChuckNorris.fact)
+end
